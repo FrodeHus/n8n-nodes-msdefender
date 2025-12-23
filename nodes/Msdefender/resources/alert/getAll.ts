250b9508bfad50bbfdbd74c5cdfa017b821a6c3e
FrodeHus <frode.hus@outlook.com>
2025-12-23 23:02:59 +0100

    Implement alert resource and add getAllAlertsDescription for expanded alert details

---
+++ nodes/Msdefender/resources/alert/getAll.ts
@@ -0,0 +1,53 @@
+import { INodeProperties } from 'n8n-workflow';
+
+const showOnlyForGetAllAlerts = {
+	operation: ['listAlerts'],
+	resource: ['alert'],
+};
+
+export const getAllAlertsDescription: INodeProperties[] = [
+	{
+		displayName: 'Expand Evidence Details',
+		name: 'expandEvidenceDetails',
+		type: 'boolean',
+		displayOptions: {
+			show: {
+				...showOnlyForGetAllAlerts,
+			},
+		},
+		default: false,
+		description: 'Whether to expand evidence details in the alert response',
+		routing: {
+			send: {
+				type: 'query',
+				property: '$expand',
+				value: 'evidence',
+			},
+		},
+	},
+	{
+		displayName: 'Limit',
+		name: 'limit',
+		type: 'number',
+		default: 50,
+		displayOptions: {
+			show: {
+				...showOnlyForGetAllAlerts,
+			},
+		},
+		typeOptions: {
+			minValue: 1,
+			maxValue: 1000,
+		},
+		description: 'Max number of results to return',
+		routing: {
+			send: {
+				type: 'query',
+				property: '$top',
+			},
+			output: {
+				maxResults: '={{$value}}',
+			},
+		},
+	}
+];
--- nodes/Msdefender/resources/alert/index.ts
+++ nodes/Msdefender/resources/alert/index.ts
@@ -1,34 +1,36 @@
 import { INodeProperties } from "n8n-workflow";
+import { getAllAlertsDescription } from './getAll';
 
 const showOnlyForAlerts = {
     resource: ['alert'],
 };
 
 export const alertDescription: INodeProperties[] = [
-    {
-        displayName: 'Operation',
-        name: 'operation',
-        type: 'options',
-        noDataExpression: true,
-        displayOptions: {
-            show: {
-                ...showOnlyForAlerts,
-            },
-        },
-        options: [
-            {
-                name: 'List Alerts',
-                action: 'List security alerts',
-                value: 'listAlerts',
-                description: 'List security alerts',
-                routing: {
-                    request: {
-                        method: 'GET',
-                        url: '/api/alerts',
-                    },
-                },
-            },
-        ],
-        default: 'listAlerts'
-    },
+	{
+		displayName: 'Operation',
+		name: 'operation',
+		type: 'options',
+		noDataExpression: true,
+		displayOptions: {
+			show: {
+				...showOnlyForAlerts,
+			},
+		},
+		options: [
+			{
+				name: 'List Alerts',
+				action: 'List security alerts',
+				value: 'listAlerts',
+				description: 'List security alerts',
+				routing: {
+					request: {
+						method: 'GET',
+						url: '/api/alerts',
+					},
+				},
+			},
+		],
+		default: 'listAlerts',
+		...getAllAlertsDescription,
+	},
 ];  

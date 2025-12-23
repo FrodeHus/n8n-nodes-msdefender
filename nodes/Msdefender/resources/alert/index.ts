0d47511fedac747400cf1d3d30a535c54a697400
FrodeHus <frode.hus@outlook.com>
2025-12-23 23:11:13 +0100

    Add 'Get Alert' operation to alert resource for retrieving security alerts by ID

--- nodes/Msdefender/resources/alert/index.ts
+++ nodes/Msdefender/resources/alert/index.ts
@@ -18,6 +18,28 @@ export const alertDescription: INodeProperties[] = [
 		},
 		options: [
 			{
+				name: 'Get Alert',
+				action: 'Get a security alert',
+				value: 'getAlert',
+				description: 'Get a security alert by ID',
+				options: [
+					{
+						displayName: 'Alert ID',
+						name: 'alertId',
+						type: 'string',
+						default: '',
+						required: true,
+						description: 'The ID of the alert to retrieve',
+					},
+				],
+				routing: {
+					request: {
+						method: 'GET',
+						url: '/api/alerts/{{ $parameter.alertId }}',
+					},
+				},
+			},
+			{
 				name: 'List Alerts',
 				action: 'List security alerts',
 				value: 'listAlerts',

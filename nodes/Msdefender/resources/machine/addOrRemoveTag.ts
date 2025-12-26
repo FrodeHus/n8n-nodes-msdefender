c0dad3bf704bd85e1a739ccce09ac7c9eff37242
FrodeHus <frode.hus@outlook.com>
2025-12-26 06:57:56 +0100

    Refactor addOrRemoveTagDescription for consistency in action values and formatting

--- nodes/Msdefender/resources/machine/addOrRemoveTag.ts
+++ nodes/Msdefender/resources/machine/addOrRemoveTag.ts
@@ -6,74 +6,74 @@ const showOnlyForAddOrRemoveTags = {
 };
 
 export const addOrRemoveTagDescription: INodeProperties[] = [
-    {
-        displayName: 'Machine ID',
-        name: 'machineId',
-        type: 'string',
-        displayOptions: {
-            show: {
-                ...showOnlyForAddOrRemoveTags,
-            },
-        },
-        default: '',
-        required: true,
-        description: 'The ID of the machine to add or remove tags for',
-        routing: {
-            request: {
-                method: 'POST',
-                url: '=/api/machines/{{ $parameter.machineId }}/tags',
-            },
-        },
-    },
-    {
-        displayName: 'Tag',
-        name: 'tag',
-        type: 'string',
-        displayOptions: {
-            show: {
-                ...showOnlyForAddOrRemoveTags,
-            },
-        },
-        default: '',
-        required: true,
-        description: 'Tag to add or remove from the machine',
-        routing: {
-            send: {
-                type: 'body',
-                property: 'Value',
-            },
-        },
-    },
-    {
-        displayName: 'Action',
-        name: 'action',
-        type: 'options',
-        displayOptions: {
-            show: {
-                ...showOnlyForAddOrRemoveTags,
-            },
-        },
-        options: [
-            {
-                name: 'Add',
-                action: 'Add tag to machine',
-                value: 'add',
-                description: 'Add the tag to the machine',
-            },
-            {
-                name: 'Remove',
-                action: 'Remove tag from machine',
-                value: 'remove',
-                description: 'Remove the tag from the machine',
-            },
-        ],
-        default: 'add',
-        description: 'Whether to add or remove the tag from the machine',
-        routing: {
-            send: {
-                type: 'body',
-                property: 'Action',
-            },
-        },
-    }
+	{
+		displayName: 'Machine ID',
+		name: 'machineId',
+		type: 'string',
+		displayOptions: {
+			show: {
+				...showOnlyForAddOrRemoveTags,
+			},
+		},
+		default: '',
+		required: true,
+		description: 'The ID of the machine to add or remove tags for',
+		routing: {
+			request: {
+				method: 'POST',
+				url: '=/api/machines/{{ $parameter.machineId }}/tags',
+			},
+		},
+	},
+	{
+		displayName: 'Tag',
+		name: 'tag',
+		type: 'string',
+		displayOptions: {
+			show: {
+				...showOnlyForAddOrRemoveTags,
+			},
+		},
+		default: '',
+		required: true,
+		description: 'Tag to add or remove from the machine',
+		routing: {
+			send: {
+				type: 'body',
+				property: 'Value',
+			},
+		},
+	},
+	{
+		displayName: 'Action',
+		name: 'action',
+		type: 'options',
+		displayOptions: {
+			show: {
+				...showOnlyForAddOrRemoveTags,
+			},
+		},
+		options: [
+			{
+				name: 'Add',
+				action: 'Add tag to machine',
+				value: 'Add',
+				description: 'Add the tag to the machine',
+			},
+			{
+				name: 'Remove',
+				action: 'Remove tag from machine',
+				value: 'Remove',
+				description: 'Remove the tag from the machine',
+			},
+		],
+		default: 'Add',
+		description: 'Whether to add or remove the tag from the machine',
+		routing: {
+			send: {
+				type: 'body',
+				property: 'Action',
+			},
+		},
+	},
 ];

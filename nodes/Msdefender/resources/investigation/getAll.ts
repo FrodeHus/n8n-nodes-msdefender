065e6fdfc3dfa30433c74b4e8376eb0cd618a2ca
Frode Hus <frode.hus@outlook.com>
2026-01-06 17:42:41 +0100

    Refactor alert, investigation, machine, machine action, remediation, and vulnerability nodes to use OData operators for filtering, limiting, and skipping results

--- nodes/Msdefender/resources/investigation/getAll.ts
+++ nodes/Msdefender/resources/investigation/getAll.ts
@@ -1,4 +1,4 @@
-import { IExecuteSingleFunctions, IHttpRequestOptions, INodeProperties } from 'n8n-workflow';
+import { INodeProperties } from 'n8n-workflow';
 
 const showOnlyForGetAllInvestigations = {
 	operation: ['getAllInvestigations'],
@@ -7,31 +7,56 @@ const showOnlyForGetAllInvestigations = {
 
 export const getAllInvestigationsDescription: INodeProperties[] = [
 	{
-		displayName: 'Filter',
-		name: 'filter',
-		type: 'string',
+		displayName: 'OData Operators',
+		name: 'odataOperators',
+		type: 'collection',
 		displayOptions: {
-			show: {
-				...showOnlyForGetAllInvestigations,
-			},
+			show: showOnlyForGetAllInvestigations,
 		},
-		default: '',
-		description: 'OData filter to apply to the investigations list',
-		routing: {
-			send: {
-				type: 'query',
-				property: '$filter',
-				preSend: [
-					async function (
-						this: IExecuteSingleFunctions,
-						requestOptions: IHttpRequestOptions,
-					): Promise<IHttpRequestOptions> {
-						if (requestOptions.qs && this.getNodeParameter('filter', '') === '')
-							delete requestOptions.qs['$filter'];
-						return requestOptions;
+		default: {},
+		placeholder: 'Add OData Operator',
+		options: [
+			{
+				displayName: 'Filter',
+				name: 'filter',
+				type: 'string',
+				default: '',
+				description: 'An OData filter expression that filters elements in the collection. Available fields: startTime, ID, state, machineId, and triggeringAlertId.',
+				routing: {
+					send: {
+						type: 'query',
+						property: '$filter',
 					},
-				],
+				},
 			},
-		},
+			{
+				displayName: 'Top',
+				name: 'top',
+				type: 'string',
+				default: '',
+				description:
+					'An OData top expression that limits the number of elements in the collection. See Microsoft documentation for supported syntax.',
+				routing: {
+					send: {
+						type: 'query',
+						property: '$top',
+					},
+				},
+			},
+			{
+				displayName: 'Skip',
+				name: 'skip',
+				type: 'string',
+				default: '',
+				description:
+					'An OData skip expression that skips a number of elements in the collection. See Microsoft documentation for supported syntax.',
+				routing: {
+					send: {
+						type: 'query',
+						property: '$skip',
+					},
+				},
+			},
+		],
 	},
 ];

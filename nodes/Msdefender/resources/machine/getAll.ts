a8715f7e144e8333b6abc76b2252b91d6729eb1c
frodehus <frode.hus@outlook.com>
2026-01-08 07:28:53 +0100

    Refactor OData operator handling across alert, investigation, machine, software, and vulnerability nodes to utilize common properties for improved consistency and maintainability.

--- nodes/Msdefender/resources/machine/getAll.ts
+++ nodes/Msdefender/resources/machine/getAll.ts
@@ -1,4 +1,5 @@
 import type { INodeProperties } from 'n8n-workflow';
+import { commonOdataProperties } from '../../shared/odataProperties';
 
 const showOnlyForGetAllMachine = {
 	operation: ['getAllMachines'],
@@ -7,136 +8,9 @@ const showOnlyForGetAllMachine = {
 
 export const getAllMachineDescription: INodeProperties[] = [
 	{
-		displayName: 'OData Operators',
-		name: 'odataOperators',
-		type: 'collection',
+		...commonOdataProperties,
 		displayOptions: {
 			show: showOnlyForGetAllMachine,
-		},
-		default: {},
-		placeholder: 'Add OData Operator',
-		options: [
-			{
-				displayName: 'Filter',
-				name: 'filter',
-				type: 'string',
-				default: '',
-				description: 'An OData filter expression that filters elements in the collection. Available fields: computerDnsName, ID, version, deviceValue, aadDeviceId, machineTags, lastSeen,exposureLevel, onboardingStatus, lastIpAddress, healthStatus, osPlatform, riskScore and rbacGroupId.',
-				routing: {
-					send: {
-						type: 'query',
-						property: '$filter',
-					},
-				},
-			},
-			{
-				displayName: 'Top',
-				name: 'top',
-				type: 'string',
-				default: '',
-				description:
-					'An OData top expression that limits the number of elements in the collection. See Microsoft documentation for supported syntax.',
-				routing: {
-					send: {
-						type: 'query',
-						property: '$top',
-					},
-				},
-			},
-			{
-				displayName: 'Skip',
-				name: 'skip',
-				type: 'string',
-				default: '',
-				description:
-					'An OData skip expression that skips a number of elements in the collection. See Microsoft documentation for supported syntax.',
-				routing: {
-					send: {
-						type: 'query',
-						property: '$skip',
-					},
-				},
-			},
-		],
-	},
-	{
-		displayName: 'Limit',
-		name: 'limit',
-		type: 'number',
-		default: 50,
-		displayOptions: {
-			show: {
-				...showOnlyForGetAllMachine,
-				returnAll: [false],
-			},
-		},
-		typeOptions: {
-			minValue: 1,
-			maxValue: 1000,
-		},
-		description: 'Max number of results to return',
-		routing: {
-			send: {
-				type: 'query',
-				property: '$top',
-			},
-			output: {
-				maxResults: '={{$value}}',
-			},
-		},
-	},
-	{
-		displayName: 'Return All',
-		name: 'returnAll',
-		type: 'boolean',
-		displayOptions: {
-			show: {
-				...showOnlyForGetAllMachine,
-			},
-		},
-		default: false,
-		description: 'Whether to return all results or only up to a given limit',
-		routing: {
-			send: {
-				paginate: '={{ $value }}',
-				type: 'query',
-				property: '$top',
-				value: '={{parameter.limit}}',
-			},
-			operations: {
-				pagination: {
-					type: 'generic',
-					properties: {
-						continue: `={{ !!($response.body['@odata.nextLink']) }}`,
-						request: {
-							url: `={{ $request.url }}`,
-						},
-					},
-				},
-			},
-		},
-	},
-	{
-		displayName: 'Skip',
-		name: 'skip',
-		type: 'number',
-		displayOptions: {
-			show: {
-				...showOnlyForGetAllMachine,
-				returnAll: [false],
-			},
-		},
-		typeOptions: {
-			minValue: 0,
-			maxValue: 1000,
-		},
-		default: 0,
-		description: 'Number of results to skip',
-		routing: {
-			send: {
-				type: 'query',
-				property: '$skip',
-			},
 		},
 	},
 ];
--- nodes/Msdefender/resources/software/getAllSoftware.ts
+++ nodes/Msdefender/resources/software/getAllSoftware.ts
@@ -1,4 +1,5 @@
 import { INodeProperties } from 'n8n-workflow';
+import { commonOdataProperties } from '../../shared/odataProperties';
 
 const showOnlyForGetAllSoftware = {
 	operation: ['getAllSoftware'],
@@ -7,65 +8,11 @@ const showOnlyForGetAllSoftware = {
 
 export const getAllSoftwareDescription: INodeProperties[] = [
 	{
-		displayName: 'OData Operators',
-		name: 'odataOperators',
-		type: 'collection',
+		...commonOdataProperties,
 		displayOptions: {
 			show: {
 				...showOnlyForGetAllSoftware,
 			},
 		},
-		default: {},
-		description: 'OData operators to filter the software results',
-		placeholder: 'Add OData Operators',
-		options: [
-			{
-				displayName: 'Filter',
-				name: 'filter',
-				type: 'string',
-				default: '',
-				routing: {
-					request: {
-						qs: {
-							$filter: '={{$value}}',
-						},
-					},
-				},
-				description:
-					'An OData filter expression that filters elements in the collection. Allowed values: ID, name and vendor.',
-			},
-			{
-				displayName: 'Top',
-				name: '$top',
-				type: 'number',
-				typeOptions: {
-					minValue: 1,
-					maxValue: 100,
-				},
-				default: 50,
-				description: 'Specifies the maximum number of items to return from the collection',
-				routing: {
-					request: {
-						qs: {
-							$top: '={{$value}}',
-						},
-					},
-				},
-			},
-			{
-				displayName: 'Skip',
-				name: '$skip',
-				type: 'number',
-				default: 0,
-				description: 'Specifies the number of items to skip in the collection',
-				routing: {
-					request: {
-						qs: {
-							$skip: '={{$value}}',
-						},
-					},
-				},
-			},
-		],
 	},
 ];

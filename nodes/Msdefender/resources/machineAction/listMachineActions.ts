7f6ef746cf3b06f2b46885ed520e5d581740ae99
frodehus <frode.hus@outlook.com>
2026-01-08 07:42:28 +0100

    refactor: streamline OData operator handling in listMachineActions and getRecommendations nodes for improved maintainability

--- nodes/Msdefender/resources/machineAction/listMachineActions.ts
+++ nodes/Msdefender/resources/machineAction/listMachineActions.ts
@@ -1,4 +1,5 @@
 import { INodeProperties } from 'n8n-workflow';
+import { commonOdataProperties } from '../../shared/odataProperties';
 
 const showOnlyForListMachineActions = {
 	operation: ['listMachineActions'],
@@ -7,57 +8,9 @@ const showOnlyForListMachineActions = {
 
 export const listMachineActionsDescription: INodeProperties[] = [
 	{
-		displayName: 'OData Operators',
-		name: 'odataOperators',
-		type: 'collection',
+		...commonOdataProperties,
 		displayOptions: {
 			show: showOnlyForListMachineActions,
 		},
-		default: {},
-		placeholder: 'Add OData Operator',
-		options: [
-			{
-				displayName: 'Filter',
-				name: 'filter',
-				type: 'string',
-				default: '',
-				description:
-					'An OData filter expression that filters elements in the collection. Available fields: ID, status, machineId, type, requestor, and creationDateTimeUtc.',
-				routing: {
-					send: {
-						type: 'query',
-						property: '$filter',
-					},
-				},
-			},
-			{
-				displayName: 'Top',
-				name: 'top',
-				type: 'string',
-				default: '',
-				description:
-					'An OData top expression that limits the number of elements in the collection. See Microsoft documentation for supported syntax.',
-				routing: {
-					send: {
-						type: 'query',
-						property: '$top',
-					},
-				},
-			},
-			{
-				displayName: 'Skip',
-				name: 'skip',
-				type: 'string',
-				default: '',
-				description:
-					'An OData skip expression that skips a number of elements in the collection. See Microsoft documentation for supported syntax.',
-				routing: {
-					send: {
-						type: 'query',
-						property: '$skip',
-					},
-				},
-			},
-		],
 	},
 ];

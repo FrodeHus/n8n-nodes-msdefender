751ea61fbf6fe83170bc246c36cca58eeaee2cfa
FrodeHus <frode.hus@outlook.com>
2026-01-09 19:43:06 +0100

    feat: add update fields and determination options to alert update functionality

--- nodes/Msdefender/resources/alert/updateAlert.ts
+++ nodes/Msdefender/resources/alert/updateAlert.ts
@@ -1,4 +1,4 @@
-import { IExecuteSingleFunctions, IHttpRequestOptions, INodeProperties } from 'n8n-workflow';
+import { INodeProperties } from 'n8n-workflow';
 
 const showOnlyForUpdateAlert = {
 	operation: ['updateAlert'],
@@ -24,279 +24,118 @@ export const updateAlertDescription: INodeProperties[] = [
 		},
 	},
 	{
-		displayName: 'Assigned To',
-		name: 'assignedTo',
-		type: 'string',
-		default: '',
-		placeholder: 'someone@somewhere.com',
-		typeOptions: {
-			format: 'email',
-		},
+		displayName: 'Update Fields',
+		name: 'updateFields',
+		type: 'collection',
+		placeholder: 'Add Field',
+		default: {},
 		displayOptions: {
 			show: showOnlyForUpdateAlert,
 		},
-		description: 'The user assigned to the alert',
-		routing: {
-			send: {
-				type: 'body',
-				property: 'AssignedTo',
-				preSend: [
-					async function (
-						this: IExecuteSingleFunctions,
-						requestOptions: IHttpRequestOptions,
-					): Promise<IHttpRequestOptions> {
-						if (requestOptions.body && this.getNodeParameter('assignedTo', '') === '')
-							delete requestOptions.body['AssignedTo'];
-						return requestOptions;
-					},
-				],
-			},
-		},
-	},
-	{
-		displayName: 'Status',
-		name: 'Status',
-		type: 'options',
 		options: [
 			{
-				name: 'New',
-				value: 'New',
+				displayName: 'Assigned To',
+				name: 'assignedTo',
+				type: 'string',
+				default: '',
+				placeholder: 'someone@somewhere.com',
+				typeOptions: {
+					format: 'email',
+				},
+				description: 'The user assigned to the alert',
+				routing: {
+					send: {
+						type: 'body',
+						property: 'AssignedTo',
+					},
+				},
 			},
 			{
-				name: 'In Progress',
-				value: 'InProgress',
-			},
-			{
-				name: 'Resolved',
-				value: 'Resolved',
-			},
-		],
-		default: 'New',
-		required: true,
-		displayOptions: {
-			show: showOnlyForUpdateAlert,
-		},
-		description: 'The new status of the alert',
-		routing: {
-			send: {
-				type: 'body',
-				property: 'Status',
-				preSend: [
-					async function (
-						this: IExecuteSingleFunctions,
-						requestOptions: IHttpRequestOptions,
-					): Promise<IHttpRequestOptions> {
-						if (requestOptions.body && this.getNodeParameter('Status', '') === '')
-							delete requestOptions.body['Status'];
-						return requestOptions;
+				displayName: 'Classification',
+				name: 'classification',
+				type: 'options',
+				options: [
+					{
+						name: 'False Positive',
+						value: 'FalsePositive',
+					},
+					{
+						name: 'True Positive',
+						value: 'TruePositive',
+					},
+					{
+						name: 'Benign Positive',
+						value: 'InformationalExpectedActivity',
 					},
 				],
-			},
-		},
-	},
-	{
-		displayName: 'Classification',
-		name: 'classification',
-		type: 'options',
-		options: [
-			{
-				name: 'False Positive',
-				value: 'FalsePositive',
+				default: 'FalsePositive',
+				description: 'The new classification of the alert',
+				routing: {
+					send: {
+						type: 'body',
+						property: 'Classification',
+					},
+				},
 			},
 			{
-				name: 'True Positive',
-				value: 'TruePositive',
+				displayName: 'Comment',
+				name: 'comment',
+				type: 'string',
+				default: '',
+				description: 'Comments regarding the alert update',
+				routing: {
+					send: {
+						type: 'body',
+						property: 'Comment',
+					},
+				},
 			},
 			{
-				name: 'Benign Positive',
-				value: 'InformationalExpectedActivity',
+				displayName: 'Determination Name or ID',
+				name: 'determination',
+				type: 'options',
+				typeOptions: {
+					loadOptionsDependsOn: ['updateFields.classification'],
+					loadOptionsMethod: 'getDeterminationValues',
+				},
+
+				routing: {
+					send: {
+						type: 'body',
+						property: 'determination',
+					},
+				},
+				default: '',
+				description:
+					'Choose from the list, or specify an ID using an <a href="https://docs.n8n.io/code/expressions/">expression</a>',
 			},
-		],
-		default: 'FalsePositive',
-		displayOptions: {
-			show: showOnlyForUpdateAlert,
-		},
-		description: 'The new classification of the alert',
-		routing: {
-			send: {
-				type: 'body',
-				property: 'Classification',
-				preSend: [
-					async function (
-						this: IExecuteSingleFunctions,
-						requestOptions: IHttpRequestOptions,
-					): Promise<IHttpRequestOptions> {
-						if (requestOptions.body && this.getNodeParameter('Classification', '') === '')
-							delete requestOptions.body['Classification'];
-						return requestOptions;
+			{
+				displayName: 'Status',
+				name: 'Status',
+				type: 'options',
+				options: [
+					{
+						name: 'New',
+						value: 'New',
+					},
+					{
+						name: 'In Progress',
+						value: 'InProgress',
+					},
+					{
+						name: 'Resolved',
+						value: 'Resolved',
 					},
 				],
-			},
-		},
-	},
-	{
-		displayName: 'Determination',
-		name: 'Determination',
-		type: 'options',
-		options: [
-			{
-				name: 'Compromised Account',
-				value: 'CompromisedUser',
-				displayOptions: {
-					show: {
-						classification: ['TruePositive'],
-						...showOnlyForUpdateAlert,
-					},
-				},
-			},
-			{
-				name: 'Confirmed Activity',
-				value: 'ConfirmedActivity',
-				displayOptions: {
-					show: {
-						classification: ['InformationalExpectedActivity'],
-						...showOnlyForUpdateAlert,
-					},
-				},
-			},
-			{
-				name: 'Malware',
-				value: 'Malware',
-				displayOptions: {
-					show: {
-						classification: ['TruePositive'],
-						...showOnlyForUpdateAlert,
-					},
-				},
-			},
-			{
-				name: 'Malicious User Activity',
-				value: 'MaliciousUserActivity',
-				displayOptions: {
-					show: {
-						classification: ['TruePositive'],
-						...showOnlyForUpdateAlert,
-					},
-				},
-			},
-			{
-				name: 'Multistage Attack',
-				value: 'MultiStagedAttack',
-				displayOptions: {
-					show: {
-						classification: ['TruePositive'],
-						...showOnlyForUpdateAlert,
-					},
-				},
-			},
-			{
-				name: 'Not Malicious',
-				value: 'NotMalicious',
-				displayOptions: {
-					show: {
-						classification: ['FalsePositive'],
-						...showOnlyForUpdateAlert,
-					},
-				},
-			},
-			{
-				name: 'Not Enough Data To Validate',
-				value: 'InsufficientData',
-				displayOptions: {
-					show: {
-						classification: ['FalsePositive'],
-						...showOnlyForUpdateAlert,
-					},
-				},
-			},
-			{
-				name: 'Line-of-Business Application',
-				value: 'LineOfBusinessApplication',
-				displayOptions: {
-					show: {
-						classification: ['InformationalExpectedActivity'],
-						...showOnlyForUpdateAlert,
-					},
-				},
-			},
-			{
-				name: 'Other',
-				value: 'Other',
-				displayOptions: {
-					show: {
-						classification: ['InformationalExpectedActivity', 'TruePositive', 'FalsePositive'],
-						...showOnlyForUpdateAlert,
-					},
-				},
-			},
-			{
-				name: 'Phishing',
-				value: 'Phishing',
-				displayOptions: {
-					show: {
-						classification: ['TruePositive'],
-						...showOnlyForUpdateAlert,
-					},
-				},
-			},
-			{
-				name: 'Security Testing',
-				value: 'SecurityTesting',
-				displayOptions: {
-					show: {
-						classification: ['InformationalExpectedActivity'],
-						...showOnlyForUpdateAlert,
-					},
-				},
-			},
-			{
-				name: 'Unwanted Software',
-				value: 'UnwantedSoftware',
-				displayOptions: {
-					show: {
-						classification: ['TruePositive'],
-						...showOnlyForUpdateAlert,
+				default: 'New',
+				description: 'The new status of the alert',
+				routing: {
+					send: {
+						type: 'body',
+						property: 'Status',
 					},
 				},
 			},
 		],
-		default: 'Malware',
-		displayOptions: {
-			show: showOnlyForUpdateAlert,
-		},
-		description: 'The new determination of the alert',
-		routing: {
-			send: {
-				type: 'body',
-				property: 'Determination',
-				preSend: [
-					async function (
-						this: IExecuteSingleFunctions,
-						requestOptions: IHttpRequestOptions,
-					): Promise<IHttpRequestOptions> {
-						if (requestOptions.body && this.getNodeParameter('Determination', '') === '')
-							delete requestOptions.body['Determination'];
-						return requestOptions;
-					},
-				],
-			},
-		},
-	},
-	{
-		displayName: 'Comment',
-		name: 'comment',
-		type: 'string',
-		default: '',
-		required: true,
-		displayOptions: {
-			show: showOnlyForUpdateAlert,
-		},
-		description: 'Comments regarding the alert update',
-		routing: {
-			send: {
-				type: 'body',
-				property: 'Comment',
-			},
-		},
 	},
 ];

bcff72a8b8c75a55e92658298d2bea801b09f385
Frode Hus <frode.hus@outlook.com>
2026-02-10 20:02:59 +0100

    fix: update API endpoint for getting linked machines in recommendation (#50)

--- nodes/Msdefender/resources/recommendation/getMachinesByRecommendation.ts
+++ nodes/Msdefender/resources/recommendation/getMachinesByRecommendation.ts
@@ -6,23 +6,23 @@ const showOnlyForMachinesByRecommendation = {
 };
 
 export const getMachinesByRecommendationDescription: INodeProperties[] = [
-    {
-        displayName: 'Recommendation ID',
-        name: 'recommendationId',
-        type: 'string',
-        displayOptions: {
-            show: {
-                ...showOnlyForMachinesByRecommendation,
-            },
-        },
-        default: '',
-        required: true,
-        description: 'The ID of the recommendation to get linked machines for',
-        routing: {
-            request: {
-                method: 'GET',
-                url: '=/api/recommendations/{{ $parameter.recommendationId }}/machines',
-            },
-        },
-    },
+	{
+		displayName: 'Recommendation ID',
+		name: 'recommendationId',
+		type: 'string',
+		displayOptions: {
+			show: {
+				...showOnlyForMachinesByRecommendation,
+			},
+		},
+		default: '',
+		required: true,
+		description: 'The ID of the recommendation to get linked machines for',
+		routing: {
+			request: {
+				method: 'GET',
+				url: '=/api/recommendations/{{ $parameter.recommendationId }}/machineReferences',
+			},
+		},
+	},
 ];
